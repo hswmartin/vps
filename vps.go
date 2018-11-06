7823f08defecae604c43c610bc8c8aaf743419ab
hswmartin <hswmartin@gmail.com>
2018-11-06 11:13:55 +0800

    update

--- vps.go
+++ vps.go
@@ -1,52 +1,9 @@
 package main
 
-import (
-	"github.com/PuerkitoBio/goquery"
-	"log"
-	"net/http"
-	"sync"
-)
+import "vps/web"
 
 func main() {
-	log.Println("start!!!")
-	var wg sync.WaitGroup
-	wg.Add(2)
-	go func() {
-		defer wg.Done()
-		if crawl("#product3") != "缺货" {
-			http.Get("https://sc.ftqq.com/SCU6677T8dbadb7d61c94c3acaf6df175948d2b858c8e9d9be2fb.send?text=FinlandVps")
-		}
-	}()
-	go func() {
-		defer wg.Done()
-		if crawl("#product4") != "缺货" {
-			http.Get("https://sc.ftqq.com/SCU6677T8dbadb7d61c94c3acaf6df175948d2b858c8e9d9be2fb.send?text=GermanyVps")
-		}
-	}()
-
-	wg.Wait()
-	log.Println("finshed!!!")
-
-}
-func crawl(s string) string {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://t667.com/cart.php?gid=1", nil)
-	checkError(err)
-	req.Header.Add("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Mobile Safari/537.36")
-	req.Header.Add("cookie", "WHMCSHy6VuOzeyv1n=rbdj1huiuvmnrvs7r5v17dm4t2; __ancc_token=2FRidfuigi7LjIiVqLFuAQ==")
-	res, err := client.Do(req)
-	checkError(err)
-	defer res.Body.Close()
-	dom, err := goquery.NewDocumentFromReader(res.Body)
-	checkError(err)
-	var str string
-	dom.Find(s).Find("div.price").Next().Each(func(i int, selection *goquery.Selection) {
-		str = selection.Text()
-	})
-	return str
-}
-func checkError(err error) {
-	if err != nil{
-		log.Fatalln(err)
-	}
+	var t667 web.Web
+	t667 = new(web.T667)
+	t667.Crawl()
 }
